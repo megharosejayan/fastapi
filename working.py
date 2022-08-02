e2b159f02bed7d02d6590a3a9190994776ae7cce
megharosejayan <megharose15@gmail.com>
2022-08-02 17:33:32 +0900

    Post req example

--- working.py
+++ working.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI,Path
+from fastapi import FastAPI,Path,Query
 from typing import Optional
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
 
 #create an endpoint
 @app.get("/")
@@ -13,13 +20,8 @@ def about():
     return {"Data":"About"}
 
 
-inventory ={
-    1: {
-        "name": "Milk",
-        "price": 129,
-        "brand": "Regular"
-        }
-    }
+inventory ={}
+  
 
 #setting up path parameter
 @app.get("/get-item/{item_id}")
@@ -27,18 +29,42 @@ def get_item(item_id: int = Path(None, description="The ID of the item you would
     return inventory[item_id]
 
 #querry parameter
+# @app.get("/get-by-name")
+# def get_item(*,name: Optional[str] = None, test: int): #querry parameter becomes optional  by None(default)
+#     for item_id in inventory:
+#         if inventory[item_id]["name"]== name:
+#             return inventory[item_id]
+#     return {"Data":"Not found"}
+
+#querry and path parameter
+# @app.get("/get-by-name/{item_id}")
+# def get_item(*,item_id: int,name: Optional[str] = None, test: int): 
+#     for item_id in inventory:
+#         if inventory[item_id]["name"]== name:
+#             return inventory[item_id]
+#     return {"Data":"Not found"}
+
+#query
 @app.get("/get-by-name")
-def get_item(*,name: Optional[str] = None, test: int): #querry parameter becomes optional  by None(default)
+def get_item(name: str = Query(None, title="Name", description="Name of item.", max_length=10, min_length=2)): 
     for item_id in inventory:
-        if inventory[item_id]["name"]== name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data":"Not found"}
 
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int,item: Item):
+    if item_id in inventory:
+        return {"Error":"Item already exists"}
+
+    #inventory[item_id] = {"name": item.name, "brand": item.brand, "price":item.price}
+    inventory[item_id]=item
+    return inventory[item_id]
 
 #setting up multiple path parameters example
 # @app.get("/get-item/{item_id}/{name}")
 # def get_item(item_id: int, name: str=None): 
 #     return inventory[item_id]
 
-#
+
 

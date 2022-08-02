4d8a78c5bf2a695af425aaacff705586b656b9ee
megharosejayan <megharose15@gmail.com>
2022-08-02 14:26:35 +0900

    Set single and multiple path parameters

--- working.py
+++ working.py
@@ -6,3 +6,28 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"Data":"Test"}
+
+
+@app.get("/about")
+def about():
+    return {"Data":"About"}
+
+
+inventory ={
+    1: {
+        "name": "Milk",
+        "price": 129,
+        "brand": "Regular"
+        }
+    }
+
+#setting up path parameter
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int): #eg of type hint
+    return inventory[item_id]
+
+#setting up multiple path parameters example
+@app.get("/get-item/{item_id}/{name}")
+def get_item(item_id: int, name: str=None): 
+    return inventory[item_id]
+
